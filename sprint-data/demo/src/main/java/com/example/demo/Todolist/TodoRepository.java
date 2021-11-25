188f6fe584303954792032b09cb382cc2de2846c
maryam-balabeed <maryam.balabeed@tuwaiq.edu.sa>
2021-11-25 13:11:55 +0300

    ff

--- sprint-data/demo/src/main/java/com/example/demo/Todolist/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Todolist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todos, Integer> {
-
-}
